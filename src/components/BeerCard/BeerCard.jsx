63cd4f9bbedcf89758e4207dc15df7be7726e805
navina321 <81630548+navina321@users.noreply.github.com>
2023-02-16 23:34:08 +0000

    added function to show info when clicking card

--- src/components/BeerCard/BeerCard.jsx
+++ src/components/BeerCard/BeerCard.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { useState } from 'react'
 
 
 const BeerCard = ({name,tagline,image}) => {
 
+  const [showInfo, setShowInfo] = useState(false);
+
+  const handleClick = () => {
+    setShowInfo(!showInfo)
+  }
 
   const beerDisplayJSX = (
-    <div className='beer-display'>
+    <div className='beer-display' onClick={handleClick}>
       <h1 className='beer-display__title'>{name}</h1>
       <div className='beer-display__img-container'>
         <img src={image} className='beer-display__image' alt={name}/>
