53fa029809cd1a1625d867f618f1a60373a6b973
navina321 <81630548+navina321@users.noreply.github.com>
2023-02-17 15:01:28 +0000

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,30 @@ import Nav from "./containers/Nav/Nav";
 
 function App() {
   const [beers, setBeers] = useState([]);
-  // const [searchInput, setSearchInput] = useState([]);
+  //setting useState to false so there are no filters on API on page load
   const [filterByABV, setFilterByABV] = useState(false);
   const [filterByPH, setFilterByPH] = useState(false);
   const [filterClassics, setFilterClassics] = useState(false);
 
+  //renders beer API on page load with filters if needed, prevents infinite re-rendering
   useEffect(() => {
     getBeers();
   }, [filterByABV, filterByPH, filterClassics]);
 
+  //gets beers from API without filters as default
   const getBeers = async () => {
     let url = "https://api.punkapi.com/v2/beers";
 
+    //adds parameters to url to get abv > 6
     if(filterByABV) {
       url += "?abv_gt=6.0&"
     }
+    //adds parameters to url to get classics if brewed before 2010
     if(filterClassics) {
       url += "?brewed_before=01-2010&"
     }
 
+    //returns beers from API dependent on if a filter has been used
     const res = await fetch(url);
     const beerData = await res.json();
     console.log(beerData)
@@ -31,7 +36,7 @@ function App() {
   };
   
 
-  //handleSearch function
+  //handleSearch function, checks event of typing in search bar
   const handleSearch = (e) => {
     const value = e.target.value.toLowerCase();
     const filteredBeersBySearch = beers.filter((beer) =>
@@ -40,15 +45,15 @@ function App() {
     setBeers(filteredBeersBySearch);
   };
 
-  //filter by ABV function
+  //filter by ABV function checks event of selecting checkbox on filter
   const handleABVFilter = (e) => {
     setFilterByABV(e.target.checked)
   };
-  //filter by ph function
+  //filter by ph function checks event of selecting checkbox on filter(
   const handlePHFilter = (e) => {
     setFilterByPH(e.target.checked)
   };
-  //filter by classic function
+  //filter by classic function checks event of selecting checkbox on filter
   const handleClassicFilter = (e) => {
     setFilterClassics(e.target.checked)
   };
